1f345a2bbaf395fd4270bb6cc08ba56181633762
Ricardo Snoek <ricardo@q42.nl>
2018-07-28 11:18:16 +0200

    Configured the extrusion steps for D4 gear

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -609,7 +609,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.0395, 80.0395, 400.48, 99.1 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.0395, 80.0395, 400.48, 87.17 }
 
 /**
  * Default Max Feed Rate (mm/s)
