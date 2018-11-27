c502f97a985b0a63574cd61d7ba766b86278a882
Ricardo Snoek <ricardo@q42.nl>
2018-11-27 11:19:51 +0100

    Fixed the bootloading screen

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -74,9 +74,10 @@
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
 #define STRING_CONFIG_H_AUTHOR "(Ricardo Snoek, default config)" // Who made the changes.
-//#define SHOW_BOOTSCREEN
-#define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
-#define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
+#define SHOW_BOOTSCREEN
+#define CUSTOM_BUILD_VERSION "bugfix-2.0.x-bltouch"
+#define STRING_SPLASH_LINE1 CUSTOM_BUILD_VERSION        // will be shown during bootup in line 1
+#define STRING_SPLASH_LINE2 CUSTOM_MACHINE_NAME         // will be shown during bootup in line 2
 
 /**
  * *** VENDORS PLEASE READ ***
